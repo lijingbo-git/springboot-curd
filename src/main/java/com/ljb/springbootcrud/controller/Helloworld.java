c27e19bf26f908c284555cfb9f4b39d0520549aa
lijingbo <1289139546@qq.com>
2020-07-27 22:58:33 +0800

    spring-crud第二次提交，springboot配置，国际化

--- src/main/java/com/ljb/springbootcrud/controller/Helloworld.java
+++ src/main/java/com/ljb/springbootcrud/controller/Helloworld.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class Helloworld {
 
@@ -12,4 +15,16 @@ public class Helloworld {
     public String hello(){
         return "Hello World";
     }
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        //classpath:/templates/
+        return "success";
+    }
+//    @RequestMapping({"/","/index.html"})
+//    public String index(){
+//        return "/index";
+//    }
 }
